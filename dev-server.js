5c1be9a8687f8ec05ef02d27232a850d64185ffa
andrey <dontusecascade@gmail.com>
2015-08-02 01:30:28 +0500

    init

---
+++ dev-server.js
@@ -0,0 +1,10 @@
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config.js');
+var webpack = require('webpack');
+var devServer = new WebpackDevServer(
+    webpack(config),
+    {
+        contentBase: __dirname,
+        publicPath: '/dist/'
+    }
+).listen(8088, 'localhost');
