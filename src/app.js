d1c5c5b8e45d49c889b25a5cf33091bf24ad7ed6
andrey <dontusecascade@gmail.com>
2015-08-02 18:37:09 +0500

    part 1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-var React = require('react');
-
-var TodoApp = require('./components/TodoApp.react');
+import React from 'react' ;
+import {TodoApp} from './components/TodoApp.react';
 
 React.render(<TodoApp />, document.getElementById('app'));
--- src/components/TodoApp.react.js
+++ src/components/TodoApp.react.js
@@ -1,70 +1,48 @@
-/**
- * Copyright (c) 2014-2015, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
-/**
- * This component operates as a "Controller-View".  It listens for changes in
- * the TodoStore and passes the new data to its children.
- */
-
-var Footer = require('./Footer.react');
-var Header = require('./Header.react');
-var MainSection = require('./MainSection.react');
-var React = require('react');
-var TodoStore = require('../stores/TodoStore');
+import React  from 'react';
+import Footer from './Footer.react';
+import Header from './Header.react';
+import MainSection from './MainSection.react';
+import TodoStore from '../stores/TodoStore';
 
 /**
  * Retrieve the current TODO data from the TodoStore
  */
 function getTodoState() {
-  return {
-    allTodos: TodoStore.getAll(),
-    areAllComplete: TodoStore.areAllComplete()
-  };
+    return {
+        allTodos: TodoStore.getAll(),
+        areAllComplete: TodoStore.areAllComplete()
+    };
 }
 
-var TodoApp = React.createClass({
+export class TodoApp extends React.Component {
 
-  getInitialState: function() {
-    return getTodoState();
-  },
+    constructor(props) {
+        super(props);
+        this.state = getTodoState();
+    }
 
-  componentDidMount: function() {
-    TodoStore.addChangeListener(this._onChange);
-  },
+    componentWillUnmount() {
+        TodoStore.removeChangeListener(this._onChange);
+    }
 
-  componentWillUnmount: function() {
-    TodoStore.removeChangeListener(this._onChange);
-  },
+    componentDidMount() {
+        TodoStore.addChangeListener(this._onChange);
+    }
 
-  /**
-   * @return {object}
-   */
-  render: function() {
-  	return (
-      <div>
-        <Header />
-        <MainSection
-          allTodos={this.state.allTodos}
-          areAllComplete={this.state.areAllComplete}
-        />
-        <Footer allTodos={this.state.allTodos} />
-      </div>
-  	);
-  },
+    _onChange = () => {
+        this.setState(getTodoState());
+    }
 
-  /**
-   * Event handler for 'change' events coming from the TodoStore
-   */
-  _onChange: function() {
-    this.setState(getTodoState());
-  }
-
-});
-
-module.exports = TodoApp;
+    render() {
+        return (
+            <div>
+                <Header />
+                <MainSection
+                    allTodos={this.state.allTodos}
+                    areAllComplete={this.state.areAllComplete}
+                />
+                <Footer allTodos={this.state.allTodos} />
+            </div>
+        );
+    }
+}
